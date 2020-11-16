79b0c54b143518126d2ed959b67330c722f7c8da
Vladimir Andrianov <vladimirandrianov96@gmail.com>
2020-11-16 05:08:46 +0200

    Update queue reader method, use smaller dockerfile image.

--- app/Python/eventslog/server.py
+++ app/Python/eventslog/server.py
@@ -1,21 +1,13 @@
 import nsq
 
-buf = []
+def handler(message):
+    print (message.body)
+    return True
 
-def process_message(message):
-    global buf
-    message.enable_async()
-    # cache the message for later processing
-    buf.append(message)
-    if len(buf) >= 3:
-        for msg in buf:
-            print (msg)
-            msg.finish()
-        buf = []
-    else:
-        print ('deferring processing')
+r = nsq.Reader(message_handler=handler,
+        lookupd_http_addresses=['nsqlookupd:4161'],
+        topic='activated_user', channel='events-python', lookupd_poll_interval=5)
 
-r = nsq.Reader(message_handler=process_message,
-        lookupd_http_addresses=['http://nsqlookupd:4161'],
-        topic='events', channel="basic_logs_python", max_in_flight=9)
 nsq.run()
+
+
