f5699409b94b270e2cf15d76b97035e19d63e906
Vladimir Andrianov <vladimirandrianov96@gmail.com>
2020-11-16 05:27:15 +0200

    Add methods to read all queue topics.

--- app/Python/eventslog/server.py
+++ app/Python/eventslog/server.py
@@ -1,13 +1,27 @@
 import nsq
 
-def handler(message):
-    print (message.body)
+def new_user_handler(message):
+    print ("New: " + str(message.body))
     return True
 
-r = nsq.Reader(message_handler=handler,
+def deactivated_user_handler(message):
+    print ("Deactivated: " + str(message.body))
+    return True
+
+def activated_user_handler(message):
+    print ("Activated: " + str(message.body))
+    return True
+
+r1 = nsq.Reader(message_handler=new_user_handler,
         lookupd_http_addresses=['nsqlookupd:4161'],
-        topic='activated_user', channel='events-python', lookupd_poll_interval=5)
+        topic='new_user', channel='events-python', lookupd_poll_interval=15)
+
+r2 = nsq.Reader(message_handler=deactivated_user_handler,
+        lookupd_http_addresses=['nsqlookupd:4161'],
+        topic='deactivated_user', channel='deactivated_user', lookupd_poll_interval=15)
+
+r3 = nsq.Reader(message_handler=activated_user_handler,
+        lookupd_http_addresses=['nsqlookupd:4161'],
+        topic='activated_user', channel='events-python', lookupd_poll_interval=15)
 
 nsq.run()
-
-
